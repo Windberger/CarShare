a5d58f3d5522f72466fc0e7d4c142ca4b77682cd
Windberger <winmaa21@htl-kaindorf.ac.at>
2025-06-02 20:46:39 +0200

    Improved Dashboard UI

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -28,7 +28,7 @@ function Dashboard(props) {
 
     const userContext = useContext(UserContext);
     const routeContext = useContext(RouteContext);
-    if(!userContext || !routeContext) {
+    if (!userContext || !routeContext) {
         throw new Error("Context not found");
     }
     const {userId} = userContext;
@@ -37,7 +37,7 @@ function Dashboard(props) {
 
     const getRoutesForCards = (userId: number | null) => {
 
-        if(userId == null) {
+        if (userId == null) {
             console.error("User ID is null!");
         }
 
@@ -63,14 +63,14 @@ function Dashboard(props) {
     const handleJoinCarpool = () => {
         routeExists(joinCode)
             .then((existing) => {
-               if (existing == true) {
-                  navigate("/joinRoute/" + joinCode);
-               } else {
-                   alert("This route does not exist");
-               }
+                if (existing == true) {
+                    navigate("/joinRoute/" + joinCode);
+                } else {
+                    alert("This route does not exist");
+                }
             })
             .catch((error) => {
-                if(error.status == 404) {
+                if (error.status == 404) {
                     alert("A route with this code does not exist!");
                 } else {
                     alert("An unknown error occurred!");
@@ -80,61 +80,80 @@ function Dashboard(props) {
         setIsModalOpen(false);
     };
 
+    const setModalOpen = (open: boolean) => {
+        setIsModalOpen(open);
+    }
+
     useEffect(() => {
         getRoutesForCards(userId);
     }, []);
 
     return (
-        <div className={"bg-white min-h-screen w-screen"}>
-            <Navbar previousPage={null}/>
-            <div className="flex gap-4 mb-6 m-4">
-                <Link
-                    to="/createCarpool"
-                    className="flex items-center gap-2 px-4 py-2 bg-[#194569] text-white shadow rounded-3xl"
-                >
-                    <IoMdAdd/> Add Route
-                </Link>
-                <button
-                    onClick={() => setIsModalOpen(true)}
-                    className="px-4 py-2 bg-[#194569] text-white rounded-3xl shadow"
-                >
-                    Join Carpool
-                </button>
-            </div>
+        <div className="bg-white min-h-screen w-screen">
+            {
+                !userId ?
+                    <div className="text-black">
+                        <div>Please log in to access this page</div>
+                        <a onClick={() => navigate("/")} className="cursor-pointer">Back to Homepage</a>
+                    </div>
+                    :
 
-            <div className="grid grid-cols-2 gap-6">
-                <RoutesCard title={"Recent Routes"} routes={driverRoutes}></RoutesCard>
-                <RoutesCard title={"Recent Joined Routes"} routes={joinRoutes}></RoutesCard>
-            </div>
+                    <div className={"bg-white min-h-screen w-screen"}>
 
-            {isModalOpen && (
-                <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-white p-6 rounded-lg shadow-lg">
-                        <h2 className="text-lg text-black font-bold mb-4">Enter Join Code</h2>
-                        <input
-                            type="text"
-                            value={joinCode}
-                            onChange={(e) => setJoinCode(e.target.value)}
-                            className="border p-2 w-full mb-4"
-                            placeholder="Enter join code"
-                        />
-                        <div className="flex justify-between">
-                            <button
-                                onClick={() => setIsModalOpen(false)}
-                                className="px-4 py-2 bg-gray-300 rounded-lg"
+
+                        <Navbar previousPage={null}/>
+                        <div className="flex gap-4 mb-6 m-4">
+                            <Link
+                                to="/createCarpool"
+                                className="flex items-center gap-2 px-4 py-2 bg-[#194569] text-white shadow rounded-3xl"
                             >
-                                Cancel
-                            </button>
+                                <IoMdAdd/> Add Route
+                            </Link>
                             <button
-                                onClick={handleJoinCarpool}
-                                className="px-4 py-2 bg-[#194569] text-white rounded-lg"
+                                onClick={() => setIsModalOpen(true)}
+                                className="px-4 py-2 bg-[#194569] text-white rounded-3xl shadow"
                             >
-                                Join
+                                Join Carpool
                             </button>
                         </div>
+
+                        <div className="grid grid-cols-2 gap-6">
+                            <RoutesCard title={"Recent Routes"} routes={driverRoutes}
+                                        setModalOpen={setModalOpen}></RoutesCard>
+                            <RoutesCard title={"Recent Joined Routes"} routes={joinRoutes}
+                                        setModalOpen={setModalOpen}></RoutesCard>
+                        </div>
+
+                        {isModalOpen && (
+                            <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+                                <div className="bg-white p-6 rounded-lg shadow-lg">
+                                    <h2 className="text-lg text-black font-bold mb-4">Enter Join Code</h2>
+                                    <input
+                                        type="text"
+                                        value={joinCode}
+                                        onChange={(e) => setJoinCode(e.target.value)}
+                                        className="border p-2 w-full mb-4"
+                                        placeholder="Enter join code"
+                                    />
+                                    <div className="flex justify-between">
+                                        <button
+                                            onClick={() => setIsModalOpen(false)}
+                                            className="px-4 py-2 bg-gray-300 rounded-lg"
+                                        >
+                                            Cancel
+                                        </button>
+                                        <button
+                                            onClick={handleJoinCarpool}
+                                            className="px-4 py-2 bg-[#194569] text-white rounded-lg"
+                                        >
+                                            Join
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
-                </div>
-            )}
+            }
         </div>
     );
 }
